569c6485b11662a40b7535b3c58cec54792a9f57
Denys Vitali <denys@denv.it>
2026-05-02 19:34:19 +0200

    fix: initialize chakra provider

--- webui/src/main.jsx
+++ webui/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import App from './App'
 import { DeviceUrlProvider } from './DeviceContext'
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider value={defaultSystem}>
       <DeviceUrlProvider>
         <App />
       </DeviceUrlProvider>
